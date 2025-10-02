53c566c6d19c0b7ae24fb5a296b2f0203067228c
Sid Jr <sidalbertok@gmail.com>
2025-10-02 18:24:47 -0300

    Create Paginas.ts

---
+++ Sr_aneis_novo/Paginas.ts
@@ -0,0 +1,9 @@
+import { VerificaPagina } from "./Interface";
+
+//Classe da Pagina
+export abstract class PaginaBase implements VerificaPagina{
+    constructor(public paginaNumero: number,public paginaInfo: string) {}
+    Verificarpagina(): void {
+        console.log(`Página ${this.paginaNumero}: ${this.paginaInfo}`);
+    }
+}
