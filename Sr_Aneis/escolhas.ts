7d9655cef60dee44caa5259f5ff2babdf6e46709
Sid Jr <sidalbertok@gmail.com>
2025-10-01 21:18:47 -0300

    Update escolhas.ts

--- Sr_Aneis/escolhas.ts
+++ Sr_Aneis/escolhas.ts
@@ -1,7 +1,20 @@
+import { VerificaPagina } from "./Interface";
 import { PaginaBase } from "./Pagina";
-import { Personagem } from "./InfoPersonagem";
 
+export class EscolhasJogador extends PaginaBase implements VerificaPagina {
+    constructor(
+        numero: number,
+        texto: string,
+        public escolhas: { descricao: string; proximaPagina: number }[]
+    ) {
+        super(numero, texto);
+    }
 
-class Escolha{
-    
+    override Verificarpagina(): void {
+        super.Verificarpagina();
+        console.log("Escolhas disponíveis:");
+        this.escolhas.forEach((e, i) => {
+            console.log(`${i + 1} - ${e.descricao}`); // aqui corrigi com template string
+        });
+    }
 }
